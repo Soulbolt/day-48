8783a6be2c6de822a95c717c677ccba5765bfdb1
Soulbolt <soulbolt@gmail.com>
2024-01-04 21:26:31 -0700

    created code to scrape data from wikipedia.org, Used By.ID to get the total articles number.

---
+++ interaction.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
+
+# Keep Broswer open after program runs.
+chrome_options = webdriver.ChromeOptions()
+ua = UserAgent()
+user_agent = ua.random
+# chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument(f'--user-agent={user_agent}')
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://en.wikipedia.org")
+total_articles = driver.find_element(By.ID, value="articlecount")
+print(total_articles.text.split()[0])
+
+driver.quit()
+
