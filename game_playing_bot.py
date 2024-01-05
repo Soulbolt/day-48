ee8846108d9c11291885212158e19bf49c372eb2
Soulbolt <soulbolt@gmail.com>
2024-01-05 16:34:51 -0700

    added code that finds cookie and clicks on it, finds grandma and current value as well as the current money value.

--- game_playing_bot.py
+++ game_playing_bot.py
@@ -9,3 +9,14 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+# Find cookie and click on it
+cookie = driver.find_element(By.ID, value="cookie").click()
+
+# Find the money
+money = driver.find_element(By.ID, value="money").text
+print(money)
+
+# Find Grandma and get the value
+grandma = driver.find_element(By.ID, value="buyGrandma").text.split()[2]
+print(grandma)
