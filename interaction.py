b8382e269cece7b6284df87ef1fd148def11b782
Soulbolt <soulbolt@gmail.com>
2024-01-04 21:48:37 -0700

    Added code to focus on a specific link and click on it. as well as focus to populate text on search input and hit enter to search.

--- interaction.py
+++ interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from fake_useragent import UserAgent
 
 # Keep Broswer open after program runs.
@@ -13,6 +14,15 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org")
 total_articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 print(total_articles.text)
+# Click on link
+# total_articles.click()
+# Find Element by link text
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+# Find the "Search" <input> by Name
+search = driver.find_element(By.NAME, value="search")
+# Sending keyboard input to Selenium using Keys.Enter
+search.send_keys("Python", Keys.ENTER)
 
-driver.quit()
+# driver.quit()
 
