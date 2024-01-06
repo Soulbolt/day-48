1f8b6f542a1f28e7e21b0068dba8b556bd9b9607
Soulbolt <soulbolt@gmail.com>
2024-01-05 19:52:01 -0700

    Implmeneted while loops that checks against the time the game should be running with the constant GAME_TIME_MIN and checks if statment in 5 second intervals and clicks on grandma buy/upgrade if enough currency. Bot stops once time is reached and prints out score and quits browser.

--- game_playing_bot.py
+++ game_playing_bot.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
+
+# Minutes to run and intervals to check
+GAME_TIME_MIN = 1*60
+INTERVAL_IN_SEC = time.time() +5
 # Keep Broswer open after program runs.
 chrome_options = webdriver.ChromeOptions()
 
@@ -11,12 +16,32 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 # Find cookie and click on it
-cookie = driver.find_element(By.ID, value="cookie").click()
+cookie = driver.find_element(By.ID, value="cookie")
 
-# Find the money
-money = driver.find_element(By.ID, value="money").text
-print(money)
 
-# Find Grandma and get the value
-grandma = driver.find_element(By.ID, value="buyGrandma").text.split()[2]
-print(grandma)
+# # Find the money
+# money = driver.find_element(By.XPATH, value="//*[@id='money']").text
+# print(money)
+
+# Create Loop to auto click cookie and check money value against Grandma's value, click on Grandma if money >=
+start_time = time.time()
+while (time.time() - start_time) < GAME_TIME_MIN:
+    money = driver.find_element(By.XPATH, value="//*[@id='money']").text
+    # Find Grandma and get the value
+    grandma = driver.find_element(By.ID, value="buyGrandma").text.split()[2]
+    # print(grandma)
+    click_buy_grandma = driver.find_element(By.ID, value="buyGrandma")
+    score = driver.find_element(By.ID, value="cps").text
+    # Click on cookie`
+    cookie.click()
+    # time_check = (time.time() - start_time)
+    # print(time_check)
+    # print(f"money: {int(money)}\nGrandma: {int(grandma)}")
+    if time.time() > INTERVAL_IN_SEC:
+        if int(money) > int(grandma):
+            click_buy_grandma.click()
+            time.sleep(.05)
+    else:
+        pass
+print(score)
+driver.quit()
